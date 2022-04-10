0af07810b58b9a168fdcce5ea6b35b296d4fe3e1
JavaScythe <slashmod6@gmail.com>
2022-04-10 09:26:07 -0400

    Create pr1-2.js

---
+++ ls1/pr1-2.js
@@ -0,0 +1,69 @@
+var a = 1;
+var b = 2;
+var c = a+b;
+
+// c is equal to __
+
+var a = 1;
+var b = a;
+var c = a+b;
+
+// c is equal to __
+
+var a = [1,2,3];
+
+// a[__] is equal to 3
+// a[__] is equal to 1
+// a[__] is equal to 2
+
+a[0] = a[0] + 1;
+
+// a[0] is equal to __
+
+var c = 2 + 4;
+
+// change the plus in the expression above so that c is equal to 8
+
+var c = 8 + 2;
+
+// change the plus in the expression above so that c is equal to 4
+
+var c = 8 + 4;
+
+// change the plus in the expression above so that c is equal to 4
+
+var a = "hello";
+var b = "world";
+var c = a + b;
+
+// c is equal to __
+
+var a = "hello";
+
+// a[__] is equal to "h"
+// a[__] is equal to "o"
+// a[__] is equal to "e"
+
+a[1] = "3";
+
+// a is equal to __
+
+
+
+var a = 5
+var b = 2;
+var c = a + b; // c is equal to __
+a = a*2; // a is equal to __
+a = a-7; // a is equal to __
+c = c + a; // c is equal to __
+b = c /2; // b is equal to __
+var d = [a, b, c];
+d[0] = d[0] + 1; // d[0] is equal to __
+d[1] = d[1] * 2; // d[1] is equal to __
+d[2] = d[2] - 7; // d[2] is equal to __
+
+// a is equal to __
+// b is equal to __
+// c is equal to __
+// d is equal to __
+// d should look like this: [__, __, __] (array format)
