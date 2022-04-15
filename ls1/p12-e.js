0487e1a6e97d8bf12185d519d1e7c94286695ce8
JavaScythe <slashmod6@gmail.com>
2022-04-15 08:57:17 -0400

    Add files via upload

---
+++ ls1/p12-e.js
@@ -0,0 +1,88 @@
+var a = 5;
+var b = 5;
+var c = a+b;
+
+// c is equal to __
+
+var a = 4;
+var b = a;
+var c = a+b;
+
+// c is equal to __
+
+var a = [2,1,3];
+
+// a[__] is equal to 3
+// a[__] is equal to 1
+// a[__] is equal to 2
+
+a[0] = a[0] + 1;
+
+// a[0] is equal to __
+
+var c = 2 + 4;
+
+// change the plus in the expression above so that c is equal to 8
+
+var c = 8 + 4;
+
+// change the plus in the expression above so that c is equal to 4
+
+var c = 16 / 2;
+
+// change the plus in the expression above so that c is equal to 4
+
+var a = "beans";
+var b = "yum";
+var c = a + b;
+
+// c is equal to __
+
+var a = "beans";
+
+// a[__] is equal to "h"
+// a[__] is equal to "o"
+// a[__] is equal to "e"
+
+a[2] = "@";
+
+// a is equal to __
+
+
+
+var a = 5
+var b = 2;
+var c = a + b; // c is equal to __
+a = a*2; // a is equal to __
+a = a-7; // a is equal to __
+c = c + a; // c is equal to __
+b = c /2; // b is equal to __
+var d = [a, b, c];
+d[0] = d[0] + 1; // d[0] is equal to __
+d[1] = d[1] * 2; // d[1] is equal to __
+d[2] = d[2] - 7; // d[2] is equal to __
+
+// a is equal to __
+// b is equal to __
+// c is equal to __
+// d is equal to __
+// d should look like this: [__, __, __] (array format)
+
+var a = ["beans", "yum", "tasty", "cheese"];
+
+// a[0] is equal to __
+// a[1] is equal to __
+// a[2] is equal to __
+// a[3] is equal to __
+// a[__] is equal to "cheese"
+// a[__] is equal to "yum"
+// a[__] is equal to "tasty"
+// a[__] is equal to "beans"
+
+var b = a[1]+a[3];
+// b is equal to __
+
+// to add a element to the end of an array, use the method ._ _ _ _(element);
+// to remove a element from the end of an array, use the method ._ _ _();
+// to add a element to the beginning of an array, use the method ._ _ _ _ _ _ _(element);
+// to remove a element from the beginning of an array, use the method ._ _ _ _ _();
